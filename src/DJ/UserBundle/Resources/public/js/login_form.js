815bbfcb7778848bc636e9e4abf6d1c7336c2f92
Jakub Ruttkay <jakubruttkay@gmail.com>
2014-09-02 12:53:01 +0200

    Small performance improvements.Commit before branch merging.

--- src/DJ/UserBundle/Resources/public/js/login_form.js
+++ src/DJ/UserBundle/Resources/public/js/login_form.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-    $("input").attr('autocomplete','off');
+// $(document).ready(function(){
+//     $("input").attr('autocomplete','off');
 
-	$( "input").focus(function(e) {
-  		$( e.target ).next("span").addClass( "blur" ).fadeOut( 900 );
-	});
-	$( "input" ).blur(function(e) {
-		var val=$(e.target).val();
-      	if(!val){
-      		$(  e.target  ).next("span").removeClass( "blur" ).fadeIn( 300 );
-      	}
+// 	$( "input").focus(function(e) {
+//   		$( e.target ).next("span").addClass( "blur" ).fadeOut( 900 );
+// 	});
+// 	$( "input" ).blur(function(e) {
+// 		var val=$(e.target).val();
+//       	if(!val){
+//       		$(  e.target  ).next("span").removeClass( "blur" ).fadeIn( 300 );
+//       	}
 
-	});
-	$( "span " ).click(function(e) {
+// 	});
+// 	$( "span" ).click(function(e) {
 
-  		$( e.target ).addClass( "blur" ).fadeOut( 900 );
-		$(e.target).prev("input").focus();
-	});
-});
+//   		$( e.target ).addClass( "blur" ).fadeOut( 900 );
+// 		$(e.target).prev("input").focus();
+// 	});
+// });
