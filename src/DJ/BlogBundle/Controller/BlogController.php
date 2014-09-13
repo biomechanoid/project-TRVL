4442c5c1c071f5611c11f2da6684f7096be33b75
Jakub Ruttkay <jakubruttkay@gmail.com>
2014-09-13 02:43:46 +0200

    Added frontend for blog index and section page. Added dynamicly loaded content on this pages.

--- src/DJ/BlogBundle/Controller/BlogController.php
+++ src/DJ/BlogBundle/Controller/BlogController.php
@@ -14,7 +14,7 @@ class BlogController extends Controller
 
     /**
      * @Route("/")
-     * @Template()
+     * @Template("DJMainBundle:Blog:blog_index.html.twig")
      */
     public function indexAction()
     {
@@ -22,17 +22,25 @@ class BlogController extends Controller
     }
 
     /**
-     * @Route("/{category}{trailingSlash}", requirements={"category" = "[a-zA-Z0-9-_]+", "trailingSlash" = "[/]{0,1}"}, defaults={"category" = null, "trailingSlash" = "/" })
-     * @Template()
+     * @Route("/{category}{trailingSlash}", name="blog_category", requirements={"category" = "[a-zA-Z0-9-_]+", "trailingSlash" = "[/]{0,1}"}, defaults={"category" = null, "trailingSlash" = "/" })
+     * @Template("DJMainBundle:Blog:blog_category.html.twig")
      */
     public function categoryAction($category)
     {
-        $this->assets['category'] = $this->getCategory($category);
-        $assets['category_post'] = $this->assets['category']->getPosts();
+        $this->assets['category'] = $this->getCategory($category)['categoryName'];
+        $this->assets['categies'] = $this->getCategory($category)['all_categories'];
+        $this->assets['category_posts'] = $this->assets['category']->getPosts();
+        $this->assets['category_posts'] = [];
+        foreach ($this->assets['category']->getPosts() as $value) {
+            if($value->getStatus() == 'visible' ) {
+                $this->assets['category_posts'][] = $value;
+            }
+        }
 
         return array(
                      'category' => $this->assets['category'],
-                     'posts'=>$assets['category_post']
+                     'categies' => $this->assets['categies'],
+                     'posts'=>$this->assets['category_posts']
                      );
         // $response = $this->render('DJBlogBundle:Blog:category.html.twig', array('category' => $this->assets['category'],
         //              'posts'=>$assets['category_post']));
@@ -43,12 +51,12 @@ class BlogController extends Controller
     }
 
     /**
-     * @Route("/{category}/{post}", requirements={"category" = "[a-zA-Z0-9-_]+","post" = "[a-zA-Z0-9-_]+"}, defaults={"category" = null})
-     * @Template()
+     * @Route("/{category}/{post}", name="blog_post", requirements={"category" = "[a-zA-Z0-9-_]+","post" = "[a-zA-Z0-9-_]+"}, defaults={"category" = null})
+     * @Template("DJMainBundle:Blog:blog_post.html.twig")
      */
     public function postAction($category, $post)
     {
-        $this->assets['category'] = $this->getCategory($category);
+        $this->assets['category'] = $this->getCategory($category)['categoryName'];
 
         $post = (string)trim(strtolower($post));
         $assets['post'] = [];
@@ -68,14 +76,22 @@ class BlogController extends Controller
     public function getCategory($category)
     {
         if ($category != '' ) {
-            return $this->get('doctrine')->getRepository('DJBlogBundle:Category')
-                                                    ->findOneBySlug($category);
-            if(!$category) {
+            $categoryEnity = $this->get('doctrine')->getRepository('DJBlogBundle:Category');
+            if(!$categoryEnity) {
                 $this->createNotFoundException('Category does not exists in DB!');
             }
+            $all_categories = [];
+            foreach ($categoryEnity->findAll() as $value) {
+                $all_categories[] = $value;
+            }
+
+            return array('categoryName' => $categoryEnity->findOneBySlug($category),
+                         'all_categories' => $all_categories);
 
         } else {
             throw $this->createNotFoundException('This category does not exist!');
         }
     }
+
+
 }
